5c4e327108502261cd033ea148203718ef61e963
Nicolas Marchal <marchaln@lo-login-02.leonhard.ethz.ch>
2021-06-19 23:33:32 +0200

    minor change

--- utils/loss.py
+++ utils/loss.py
@@ -94,11 +94,11 @@ class ComputeLoss:
 
         # Define criteria
         pos_weight = torch.ones([model.nc])
-        pos_weight[1]*=3
+        pos_weight[1]*=6
         BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
-        BCEcls = nn.BCEWithLogitsLoss(pos_weight=pos_weight).to(device)
+        #BCEcls = nn.BCEWithLogitsLoss(pos_weight=pos_weight).to(device)
         BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['obj_pw']], device=device))
-        #BCEobj = nn.BCEWithLogitsLoss(pos_weight=pos_weight).to(device)
+        #BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([3], device=device))
 
         # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
         self.cp, self.cn = smooth_BCE(eps=h.get('label_smoothing', 0.0))  # positive, negative BCE targets
